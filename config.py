b6a20cb8efe84771546a97d2bea3f55864bb81b8
andreaexe2 <andrea.sestini@wetechs.it>
2025-06-19 15:12:23 +0200

    Remove sensitive files from repository

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Configurazione per l'upload dei file
-UPLOAD_FOLDER = 'static/audio'
-ALLOWED_EXTENSIONS = {'mp3', 'wav', 'ogg', 'm4a'}
-DATABASE_PATH = 'helpdesk.db'
-
-# Assicurarsi che la cartella per l'upload esista
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
